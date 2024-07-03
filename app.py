3c151fdbb6632b2f4ccddc42abfe46a2b0ad8e23
Riceball <chenlei9691@gmail.com>
2024-07-03 23:04:37 +0800

    hello from vscode

---
+++ app.py
@@ -0,0 +1,10 @@
+'''
+Author: Riceball chenlei9691@gmail.com
+Date: 2024-07-03 23:01:09
+LastEditors: Riceball chenlei9691@gmail.com
+LastEditTime: 2024-07-03 23:04:09
+FilePath: /home/Code/Her/app.py
+Description: 
+
+Copyright (c) 2024 by ${chenlei9691@gmail.com}, All Rights Reserved. 
+'''
