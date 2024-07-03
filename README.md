9eed2a56f2406b2d825b7458158a77bf816f1289
Riceball <chenlei9691@gmail.com>
2024-07-03 23:30:32 +0800

    init qwen2 model

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
+<!--
+ * @Author: Riceball chenlei9691@gmail.com
+ * @Date: 2024-07-03 23:00:44
+ * @LastEditors: Riceball chenlei9691@gmail.com
+ * @LastEditTime: 2024-07-03 23:29:46
+ * @FilePath: /home/Code/Her/README.md
+ * @Description: Design Doc of Project
+ * 
+ * Copyright (c) 2024 by ${chenlei9691@gmail.com}, All Rights Reserved. 
+-->
 # Her
 Directed and Wrote by Spike Jonze, 2013, IMDB 8.0/10
+
+## Reference
+### Model
+Qwen2-1.5B
+- doc: https://qwenlm.github.io/blog/qwen2/
--- app.py
+++ app.py
@@ -2,9 +2,46 @@
 Author: Riceball chenlei9691@gmail.com
 Date: 2024-07-03 23:01:09
 LastEditors: Riceball chenlei9691@gmail.com
-LastEditTime: 2024-07-03 23:04:09
+LastEditTime: 2024-07-03 23:23:21
 FilePath: /home/Code/Her/app.py
 Description: 
 
 Copyright (c) 2024 by ${chenlei9691@gmail.com}, All Rights Reserved. 
 '''
+# Load model directly
+from modelscope import AutoModelForCausalLM, AutoTokenizer
+device = "cpu" # the device to load the model onto
+
+# Now you do not need to add "trust_remote_code=True"
+model = AutoModelForCausalLM.from_pretrained(
+    "Qwen/Qwen2-0.5B",
+    torch_dtype="auto",
+    device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2-0.5B")
+
+# Instead of using model.chat(), we directly use model.generate()
+# But you need to use tokenizer.apply_chat_template() to format your inputs as shown below
+prompt = "Give me a short introduction to large language model."
+messages = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": prompt}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True
+)
+model_inputs = tokenizer([text], return_tensors="pt").to(device)
+
+# Directly use generate() and tokenizer.decode() to get the output.
+# Use `max_new_tokens` to control the maximum output length.
+generated_ids = model.generate(
+    model_inputs.input_ids,
+    max_new_tokens=512
+)
+generated_ids = [
+    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+]
+
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
